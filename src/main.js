de1892556dc577352752620a016f14652a15eb8b
SinaBrauch <sina.brauch@gmail.com>
2023-07-03 20:51:01 +0200

    style: backgroudnImage moved to public folder for deployment

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
+import "primevue/resources/themes/saga-blue/theme.css"; //theme
+import "primevue/resources/primevue.min.css"; //core CSS
+import "primeicons/primeicons.css"; //icons
 import './assets/stylesheets/main.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue);
+
+app.mount('#app');
