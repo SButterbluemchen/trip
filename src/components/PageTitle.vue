a3f0ab001497fd8cd13d4db4e81cb3c43580f92b
SINA BRAUCH <sina.brauch@adeo.com>
2023-07-05 17:05:14 +0200

    title: title logo added and font style changed

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,12 +1,47 @@
 <script setup>
-const titleMessage = 'Beer Trip'
+const titleMessage = 'Beer Trip 2023'
 </script>
 
 <template>
-  <img alt="Beer glass" src="@/assets/images/beerGlass.png" width="60" height="55" />
-  <div>
-    <h1>{{ titleMessage }}</h1>
-  </div>
+  <article>
+    <img alt="Beer glass" src="@/assets/images/beerGlassLogo.png" width="160" height="150" />
+    <div>
+      <h1>{{ titleMessage }}</h1>
+      <p>13. - 16. Juli 2023</p>
+    </div>
+  </article>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@import '@/assets/stylesheets/_variables.scss';
+article {
+  display: flex;
+  align-items: flex-start;
+  @media only screen and (min-width: 900px) {
+    align-items: flex-end;
+  }
+  h1 {
+    margin-top: 1rem;
+    padding: 0.5rem 1rem;
+    border-top: 2px solid $yellow-color;
+    border-bottom: 2px solid $yellow-color;
+    @media only screen and (min-width: 900px) {
+      padding: 0.5rem 5rem;
+    }
+  }
+  p {
+    color: $yellow-color;
+    text-align: center;
+    font-size: 1rem;
+    padding: 0.25rem;
+  }
+  img {
+    width: 5rem;
+    height: 5.5rem;
+    @media only screen and (min-width: 900px) {
+      width: 9rem;
+      height: 9rem;
+    }
+  }
+}
+</style>
