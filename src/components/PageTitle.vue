596e322de808c01ef75ff94f5cad6b689e184e44
SINA BRAUCH <sina.brauch@adeo.com>
2023-07-07 11:22:14 +0200

    pageTitle: layout for mobile changed to put beer behind title

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -16,17 +16,29 @@ const titleMessage = 'Beer Trip 2023'
 @import '@/assets/stylesheets/_variables.scss';
 article {
   display: flex;
-  align-items: flex-start;
-  @media only screen and (min-width: 900px) {
-    align-items: flex-end;
+  align-items: center;
+  flex-direction: column;
+  @media only screen and (min-width: 600px) {
+    flex-direction: row;
+    align-items: flex-start;
   }
   h1 {
-    margin-top: 1rem;
+    margin-top: -1.5rem;
+    background: linear-gradient(
+      90deg,
+      rgba(42, 42, 42, 0) 0%,
+      rgba(21, 21, 21, 1) 31%,
+      rgba(6, 6, 6, 1) 50%,
+      rgba(26, 26, 26, 1) 69%,
+      rgba(42, 42, 42, 0) 100%
+    );
     padding: 0.5rem 1rem;
     border-top: 2px solid $yellow-color;
     border-bottom: 2px solid $yellow-color;
-    @media only screen and (min-width: 900px) {
+    @media only screen and (min-width: 600px) {
       padding: 0.5rem 5rem;
+      margin-top: 1rem;
+      background: transparent;
     }
   }
   p {
@@ -39,8 +51,9 @@ article {
     width: 5rem;
     height: 5.5rem;
     @media only screen and (min-width: 900px) {
-      width: 9rem;
+      width: 8.5rem;
       height: 9rem;
+      padding-bottom: 1rem;
     }
   }
 }
